1ad43b4ec8acbfad8914991ce92b52e6de6ff85c
XimaDK <kadyshev_dima_2@mail.ru>
2024-01-14 19:17:20 +0700

    фишки не накладываются друг на друга, но остаются с прошлых ходов

--- src/main/java/com/example/monopolia/gameplay/Cell.kt
+++ src/main/java/com/example/monopolia/gameplay/Cell.kt
@@ -1,14 +1,12 @@
 package com.example.monopolia.gameplay
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.monopolia.R
@@ -21,6 +19,7 @@ class CellView(context: Context) {
     private val rootView: View = LayoutInflater.from(context).inflate(R.layout.cell_view, null)
     private val imageView: ImageView = rootView.findViewById(R.id.cell_image)
     private val textView: TextView = rootView.findViewById(R.id.cell_text)
+    private val chipStackLayout: LinearLayout = rootView.findViewById(R.id.chipStackLayout)
 
 
     fun setDirection(direction: Int) {
@@ -51,17 +50,21 @@ class CellView(context: Context) {
     }
 
     fun setChipVisible(visible: Boolean, playerIndex: Int, context: Context) {
-        val chipImageView = rootView.findViewById<ImageView>(R.id.game_piece1)
-        if (visible){
-            chipImageView.visibility = View.VISIBLE
-            chipImageView.setColorFilter(getPlayerColor(playerIndex, context))
-        }else{
-            chipImageView.visibility = View.INVISIBLE
+
+        val chipImageView = ImageView(context)
+        chipImageView.setImageResource(R.drawable.chip) // Use your chip image resource
+        chipImageView.setColorFilter(getPlayerColor(playerIndex, context))
+
+        if (visible) {
+            chipStackLayout.addView(chipImageView)
+        } else {
+            chipStackLayout.removeView(chipImageView)
+
         }
     }
 
+
     private fun getPlayerColor(playerIndex: Int, context : Context): Int {
-        // Choose different colors for each player
         return when (playerIndex) {
             0 -> ContextCompat.getColor(context, R.color.player1_color)
             1 -> ContextCompat.getColor(context, R.color.player2_color)
